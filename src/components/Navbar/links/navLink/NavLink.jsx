8179e2e078c00f826d6648e57a00e0742e9f0609
atulraj8804487622@gmail.com <atulraj8804487622@gmail.com>
2024-03-18 13:44:21 +0530

    Navbar Completed

---
+++ src/components/Navbar/links/navLink/NavLink.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import styles from "./navlink.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({ item }) => {
+  const pathName = usePathname();
+
+  return (
+    <Link
+      className={`${styles.container} ${
+        pathName === item.path && styles.active
+      }`}
+      href={item.path}
+    >
+      {item.title}
+    </Link>
+  );
+};
+
+export default NavLink;
